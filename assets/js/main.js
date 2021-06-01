1c178b77589e88741adcc7a985b66cb0dc628ca2
tiagorahal <rahal.aires@gmail.com>
2021-06-01 17:34:36 -0300

    Add HTML element using JavaScript test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,9 @@
+//Add window alert
 let date = new Date();
-alert(`You are accessing The Burning Wizard online store at: ${date}. The products bought will be shipped in the same day if the transaction is approved until 11AM.`); 
+alert(`You are accessing The Burning Wizard online store at: ${date}. The products bought will be shipped in the same day if the transaction is approved until 11AM. By cliking Ok you agree with our terms`); 
+
+//Change HTML 
+let footerTesting = document.createElement('p');
+footerTesting.id = 'text-js=footer';
+footerTesting.innerHTML = 'This is just a test.';
+document.getElementById('testing-js').appendChild(footerTesting);
