3e87972d90e33c27857ab696f917b1262e9662e9
tiagorahal <rahal.aires@gmail.com>
2021-06-01 16:47:54 -0300

    Add alert window to main page for testing

---
+++ assets/js/main.js
@@ -0,0 +1,2 @@
+let date = new Date();
+alert(`You are accessing The Burning Wizard online store at: ${date}. The products bought will be shipped in the same day if the transaction is approved until 11AM.`); 
