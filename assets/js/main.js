9eee1f4cf87ee579352b5f43faf1c4ec36c229b2
tiagorahal <rahal.aires@gmail.com>
2021-06-05 14:47:38 -0300

    Coment all JavaScript test content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+/*
+
 //Add window alert
 let date = new Date();
 alert(`You are accessing The Burning Wizard online store at: ${date}. The products bought will be shipped in the same day if the transaction is approved until 11AM. By cliking Ok you agree with our terms`); 
@@ -30,3 +32,4 @@ buttonToChange.onmouseout = changeBackground;
 
 buttonToChange.onkeypress = changeBackground;
 
+*/
